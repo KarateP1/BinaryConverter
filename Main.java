38986ce49529d76331231ec308ec73ac37bd8147
KarateP1 <Bannaflash08@gmail.com>
2025-09-03 22:06:27 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
-public class App {
+import java.util.*;
+import java.io.*;
+public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Are you Converting or Reverting");
+        String ans = in.next().toLowerCase();
+        
+        if(ans.equals("convert")){
+            System.out.println("Please enter the statment that you wish to convert.");
+
+        }
+        else if(ans.equals("revert")){
+            System.out.println("Please enter the statment that you wish to revert.");
+        }
+        else{
+            System.out.println("Please use a valid answer");
+        }
     }
 }
