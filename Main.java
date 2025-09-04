182f5202e3fb296956de1e0ee5a70197cc0004cc
KarateP1 <Bannaflash08@gmail.com>
2025-09-04 10:14:21 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
+
 import java.util.*;
 import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
         System.out.println("Are you Converting or Reverting");
-        String ans = in.next().toLowerCase();
+        String ans = in.nextLine().toLowerCase();
         
         if(ans.equals("convert")){
             System.out.println("Please enter the statment that you wish to convert.");
+            Converter x = new Converter();
+            String sentence = in.nextLine();
+            x.convert(sentence);
 
         }
         else if(ans.equals("revert")){
